23664be6c567f11e9d0858fc93e3a26b8bcefa1f
apreeti-tw <apreeti@thoughtworks.com>
2021-05-27 17:52:06 +0530

    Build test runner and step definition file for the add place api

---
+++ src/test/java/cucumber/options/TestRunner.java
@@ -0,0 +1,13 @@
+package cucumber.options;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "src/test/java/stepDefinitions"
+)
+public class TestRunner {
+}
---
+++ src/test/java/stepDefinitions/PlaceStepDefinitions.java
@@ -0,0 +1,37 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PlaceStepDefinitions {
+    @Given("Add Place payload")
+    public void add_place_payload() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User calls {string} api with post request")
+    public void user_calls_api_with_post_request(String api) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("API call should be successful with status code {string}")
+    public void api_call_should_be_successful_with_status_code(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("status message should contain {string}")
+    public void status_message_should_contain(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("{string} in status response should be {string}")
+    public void in_status_response_should_be(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
