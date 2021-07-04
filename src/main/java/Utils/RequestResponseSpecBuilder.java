314f43b31d6ca3f0fc5bd2f3e5ecae8573482020
apreeti-tw <apreeti@thoughtworks.com>
2021-07-05 01:07:34 +0530

    Get response and request spec from Utils file

--- src/main/java/Utils/RequestResponseSpecBuilder.java
+++ src/main/java/Utils/RequestResponseSpecBuilder.java
@@ -1,11 +1,23 @@
 package Utils;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 public class RequestResponseSpecBuilder {
     public static RequestSpecification getRequestSpec(){
-        return new RequestSpecBuilder().setBaseUri("https://reqres.in/").setContentType(ContentType.JSON).build();
+        return new RequestSpecBuilder()
+                .setBaseUri("https://reqres.in/")
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getResponseSpec(){
+        return new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
     }
 }
--- src/test/java/stepDefinitions/PlaceStepDefinitions.java
+++ src/test/java/stepDefinitions/PlaceStepDefinitions.java
@@ -9,29 +9,37 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class PlaceStepDefinitions {
     RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
     TestDataBuilder data = new TestDataBuilder();
+    RequestSpecification request;
     Response response;
 
     @Given("Add Place payload")
     public void add_place_payload() {
         requestSpecification = RequestResponseSpecBuilder.getRequestSpec();
+        responseSpecification = RequestResponseSpecBuilder.getResponseSpec();
+        request = given()
+                    .spec(requestSpecification)
+                    .body(data.getPostUserData());
     }
 
     @When("User calls {string} api with post request")
     public void user_calls_api_with_post_request(String api) {
-        response = RestAssured.given()
-                .spec(requestSpecification)
-                .body(data.getPostUserData())
-                .when()
-                .post("/api/users")
-                .then()
-                .extract()
-                .response();
+
+        response = request
+                    .when()
+                    .post("/api/users")
+                    .then()
+                    .spec(responseSpecification)
+                    .extract()
+                    .response();
     }
 
     @Then("API call should be successful with status code {int}")
