8d4cc4ca2600a18723fb3ca996306180a4874538
apreeti-tw <apreeti@thoughtworks.com>
2021-07-05 16:18:33 +0530

    Add log file to insert request and response logs

--- src/main/java/Utils/RequestResponseSpecBuilder.java
+++ src/main/java/Utils/RequestResponseSpecBuilder.java
@@ -2,18 +2,23 @@ package Utils;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class RequestResponseSpecBuilder {
     public static RequestSpecification getRequestSpec() throws IOException {
         return new RequestSpecBuilder()
                 .setBaseUri(Properties.properties().getProperty("base_url"))
                 .setContentType(ContentType.JSON)
+                .addFilter(RequestLoggingFilter.logRequestTo(new PrintStream(System.getProperty("user.dir")+"/log.txt")))
+                .addFilter(ResponseLoggingFilter.logResponseTo(new PrintStream(System.getProperty("user.dir")+"/log.txt")))
                 .build();
     }
 
